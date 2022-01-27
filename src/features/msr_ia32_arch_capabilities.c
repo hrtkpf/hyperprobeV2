d7e23577e0b89708910f5d72c1f2b7d8372a667f
Jan Hartkopf <j@hartkopf.io>
2022-01-27 12:29:33 +0100

    execute feature test for MSR IA32_ARCH_CAPABILITIES on Intel only
    Support for this MSR via CPUID was added in 4.15 for Intel only [1].
    4.17 added CPUID support for all platforms [2], but we already have
    a reliable test for that version.
    
    Note that [2] only applies to CPUID support. Proper MSR emulation
    for AMD was first added in 5.1 [3].
    A bug test could probably be created from that, but would work only
    on AMD platforms.
    
    [1]: https://git.kernel.org/pub/scm/virt/kvm/kvm.git/commit/?id=28c1c9fabf48d6ad596273a11c46e0d0da3e14cd
    [2]: https://git.kernel.org/pub/scm/virt/kvm/kvm.git/commit/?id=1eaafe91a0df4157521b6417b3dd8430bf5f52f0
    [3]: https://github.com/torvalds/linux/commit/0cf9135b773bf32fba9dd8e6699c1b331ee4b749

--- src/features/msr_ia32_arch_capabilities.c
+++ src/features/msr_ia32_arch_capabilities.c
@@ -4,11 +4,20 @@
 #include "hyperprobe/cpuid.h"
 
 // 0x7 is the CPUID Leaf written to EAX
-// if MSR IA32_ARCH_CAPABILITIES is supported, bit 29 in EDX should be 1
+// If MSR IA32_ARCH_CAPABILITIES is supported, bit 29 in EDX should be 1
+// This test only executes on Intel platforms because 4.15 introduced MSR CPUID support for Intel only
 int test_msr_ia32_arch_capabilities(){
     int a, b, c, d;
 
     DPRINTF("Starting MSR IA32_ARCH_CAPABILITIES test...\n");
+
+    cpuid(0x0, a, b, c, d);
+
+    if(c != 0x6c65746e){
+        // We are not on Intel
+        DPRINTF("Skipping test as this is not an Intel platform\n");
+        return -1;
+    }
 
     cpuid(0x7, a, b, c, d);
     DPRINTF("CPUID executed\n");
